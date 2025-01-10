b3f02752b393417bb787eb71242d3bdd3b76261e
Milen Nikolov <milenski1987@icloud.com>
2025-01-10 22:44:41 +0200

    Update

--- figure_test.py
+++
@@ -1,8 +0,0 @@
-width = int(input())
-height = int(input())
-
-for i in range(1, height + 1):
-    if i == 1 or i == height:
-        print(width * "*")
-    else:
-        print("*" + (width - 2) * " " + "*")
